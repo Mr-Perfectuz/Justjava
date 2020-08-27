82c68cb99d64470c49f3ee7d5e393ba31917f6d4
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-08-28 01:26:17 +0500

    CheckBox has been added

--- Justjava3/app/src/main/java/android/example/justjava/MainActivity.java
+++ Justjava3/app/src/main/java/android/example/justjava/MainActivity.java
@@ -11,9 +11,14 @@ package android.example.justjava;
 
 
 import java.text.NumberFormat;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,57 +26,80 @@ import androidx.appcompat.app.AppCompatActivity;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    CheckBox checkBox;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkBox = (CheckBox)findViewById(R.id.whipped_cream_checkBox);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()){
+                    Toast.makeText(getApplicationContext(), "Checked", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
     }
 
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
 
     int quantity =0;
 
     public void decrement(View view) {
         quantity = quantity -1;
-        display(quantity);
+        displayQuantity(quantity);
     }
+
 
     public void increment(View view) {
         quantity = quantity +1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view) {
-        int price = 5* quantity;
-        String message = "Total: $" + price + "\nThank you !";
-        displayMessage(message);
-//        displayPrice(5 * quantity);
+    public String createOrderSummary(int price, boolean addWhippedCream) {
+        String priceMessage = "Name: Anvar Ziyodov";
+        priceMessage += "\nAdd Whipped Cream ? " + addWhippedCream;
+        priceMessage += "\nQuantity " +quantity;
+        priceMessage += "\nTotal:" + price;
+        priceMessage +=  "\nThank you !";
+        return priceMessage;
+    }
 
+    public void submitOrder(View view) {
+
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkBox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        Log.v("MainActivity", "Has whipped Cream : "+ hasWhippedCream);
+
+        int price = calculatePrice();
+        displayMessage(createOrderSummary(price, hasWhippedCream));
 }
+
+    private int calculatePrice() {
+        return quantity * 5 ;
+
+    }
+
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView  orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
 }
